c30b643ee00be487e0ca4fac84d2859e6cd8d79c
Joe Bosman <joseph.a.bosman@gmail.com>
2016-08-31 18:13:34 -0400

    Try and crack this suckers

--- starting-point/server/auth/google.oauth.js
+++ starting-point/server/auth/google.oauth.js
@@ -4,6 +4,9 @@ var router = require('express').Router();
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+var secret = require('../../../starting-point/secret.js');
+
+
 var User = require('../api/users/user.model');
 
 router.get('/', passport.authenticate('google', {
@@ -15,11 +18,8 @@ router.get('/callback', passport.authenticate('google', {
   failureRedirect: '/signup'
 }));
 
-passport.use(new GoogleStrategy({
-  clientID: '238524570915-ivf9lnhm9bsfq13cle5ap8s28d4lmhrp.apps.googleusercontent.com',
-  clientSecret: 'GST6VQnVmhx1YIB1vDXXB3PF',
-  callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
-}, function (token, refreshToken, profile, done) {
+passport.use(new GoogleStrategy( secret.google
+  , function (token, refreshToken, profile, done) {
   var info = {
     name: profile.displayName,
     // google may not provide an email, if so we'll just fake it
